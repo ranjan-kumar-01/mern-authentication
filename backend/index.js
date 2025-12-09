7a9ea5bfaefadd78a45cd80d237e9dffba4b889e
Ranjan Kumar <ranjankumaratimi@gmail.com>
2025-12-10 00:42:25 +0530

    just formate the code alignment

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
-import express from "express"
+import express from "express";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
